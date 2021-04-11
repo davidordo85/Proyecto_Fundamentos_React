bcd25606955d9af540043f369200a00af367a9eb
David Ordoñez <davidordonezescacena@hotmail.es>
2021-04-11 04:28:34 +0200

    cambiar el estado con una función

--- src/components/auth/LoginPage/LoginForm.js
+++ src/components/auth/LoginPage/LoginForm.js
@@ -6,6 +6,21 @@ import FormField from '../../shared/FormField';
 import './LoginForm.css';
 
 function LoginForm() {
+
+    const [credentials, setCredentials] = React.useState({
+        email: '',
+        password: '',
+    });
+
+    const handleChange = event => {
+        setCredentials(oldCredentials => {
+            const newCredentials = {
+                ...oldCredentials,
+                [event.target.name]: event.target.value,
+            };
+            return newCredentials;
+        });
+    };
   return (
     <form className="loginForm">
       <FormField
@@ -13,12 +28,16 @@ function LoginForm() {
         name="email"
         label="phone, email or username"
         className="loginForm-field"
+        value={credentials.username}
+        onChange={handleChange}
       />
       <FormField
         type="password"
         name="password"
         label="password"
         className="loginForm-field"
+        value={credentials.username}
+        onChange={handleChange}
       />
       <Button type="submit" className="loginForm-submit" variant="primary">
         Log in
@@ -26,5 +45,4 @@ function LoginForm() {
     </form>
   );
 }
-
 export default LoginForm;
